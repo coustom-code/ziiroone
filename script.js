9bcbb2edf37c7ed8ba8653e466546f276a3dfc39
projectvrk <46972569+coustom-code@users.noreply.github.com>
2024-08-22 18:47:54 +0530

    adding code for matrix-animation

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+(function() {
+    const script1 = document.createElement('script');
+    script1.src = "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+    document.head.appendChild(script1);
 
+    script1.onload = function() {
+        const script2 = document.createElement('script');
+        script2.src = "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+        document.head.appendChild(script2);
+
+        script2.onload = function() {
+            // Load the custom control script from your local server
+            const customScript = document.createElement('script');
+            customScript.src = "http://localhost:5500/js/form-control.js"; // Point to your local server
+            document.head.appendChild(customScript);
+        };
+    };
+})();
